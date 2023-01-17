f30777bedc55c05a9de3da0343eb97a8200ab0a2
Alvaro Toconas <alvarotoconas@indigital.pe>
2023-01-17 00:01:57 -0500

    feat: #0003 Logging functioning correctly with boolean stubbing

--- src/main/java/pe/memo/memoflashcardsbe/authentication/controller/AuthenticationController.java
+++ src/main/java/pe/memo/memoflashcardsbe/authentication/controller/AuthenticationController.java
@@ -1,13 +1,15 @@
 package pe.memo.memoflashcardsbe.authentication.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import pe.memo.memoflashcardsbe.authentication.business.input.AuthenticationService;
 import pe.memo.memoflashcardsbe.authentication.controller.request.SignInRequest;
 
-@Controller("/auth")
+@RestController(value = "/auth")
+@Slf4j
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -19,6 +21,7 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public String signIn(@RequestBody SignInRequest request) {
+        log.info("Attempting sign in for user {}", request.username());
         boolean success = this.authenticationService.login(request.username(), request.password());
         return Boolean.toString(success);
     }
--- src/main/java/pe/memo/memoflashcardsbe/config/SecurityConfig.java
+++ src/main/java/pe/memo/memoflashcardsbe/config/SecurityConfig.java
@@ -1,15 +1,19 @@
 package pe.memo.memoflashcardsbe.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 @EnableWebSecurity
 @Component
 public class SecurityConfig {
-
 
     final PasswordEncoder passwordEncoder;
 
@@ -23,6 +27,22 @@ public class SecurityConfig {
             throws Exception {
         auth.inMemoryAuthentication().withUser("user")
                 .password(passwordEncoder.encode("password")).roles("USER");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST)
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
     }
 
 }
